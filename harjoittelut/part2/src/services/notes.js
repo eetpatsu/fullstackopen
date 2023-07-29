1580f2592584cab12ed33a9fac5b07a516040653
eetpatsu <eetu.p.sutinen@student.jyu.fi>
2023-07-29 16:10:18 +0300

    osa3b harjoittelua

--- harjoittelut/part2/src/services/notes.js
+++ harjoittelut/part2/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -21,8 +21,6 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { 
-  getAll, 
-  create, 
-  update 
-}
+const noteService = { getAll, create, update }
+
+export default noteService
